880fb1901c533569973f1e530cbf10a9de693260
Leonardo Cunha <leonardovalcesio@gmail.com>
2024-07-24 04:12:20 -0300

    FIX BUGSMAJOR

--- my-app/axios.ts
+++ my-app/axios.ts
@@ -1,6 +1,6 @@
 import axios from "axios"
 
 export const makeRequest = axios.create({
-    baseURL: 'http://localhost:8000/api/',
+    baseURL: process.env.BACKEND_URL || 'http://localhost:8000/api/',
     withCredentials: true
     })
