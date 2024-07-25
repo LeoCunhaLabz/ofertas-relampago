5f10ec96658a175556526f16a7b5e7ce8148011b
Leonardo Cunha <leonardovalcesio@gmail.com>
2024-07-24 23:14:34 -0300

    FIX BUGSMAJOR3

--- api/middleware/refreshTokenValidation.js
+++ api/middleware/refreshTokenValidation.js
@@ -1,6 +1,7 @@
 import jwt from "jsonwebtoken";
 
 export const checkRefreshToken = (req, res, next) => {
+    console.log("(Refresh) Cookies recebidos:", req.headers.cookie);  // Adicione este log
     const cookies = req.headers.cookie?.split("; ");
     const authHeader = cookies?.find(cookie => cookie.startsWith("refreshToken"));
     const refresh = authHeader && authHeader.split("=")[1];
